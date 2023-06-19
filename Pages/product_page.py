4da73509e5701e597928ba77d8da9d2d5e078a95
Kasaru <artemiy064@bk.ru>
2023-06-20 01:14:39 +0300

    Login page locators and tests

---
+++ Pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        button.click()
+
+    def should_be_same_name(self):
+        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        message = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_MESSAGE)
+        assert message.text == f"{name.text} был добавлен в вашу корзину.", "message is not equal"
+
+    def should_be_same_price(self):
+        price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        message_price = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_PRICE_MESSAGE)
+        assert price.text == message_price.text, "price is not equal"
