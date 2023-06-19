447b78dd47e01bdaa205a4ee1ef30cf227d286d1
Kasaru <artemiy064@bk.ru>
2023-06-20 02:20:46 +0300

    Для нашего интернет-магазина было запущено несколько новых промо-акций, одна из которых привела к появлению бага. Промо-акция включается путем добавления параметра ?promo=offerN к ссылке на товар.
     Вам нужно определить, при каком значении параметра promo автотест упадет. Для этого проверьте результат работы PyTest и найдите url, на котором произошла ошибка. Значение параметра может изменяться от offer0 до offer9.
    
    После того как вы обнаружили баг, учитывая что чинить его не собираются, лучше всего пометить падающий тест как xfail или skip.

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -5,7 +5,7 @@ from selenium.common.exceptions import NoAlertPresentException
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=2):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
